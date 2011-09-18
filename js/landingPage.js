ff3e5ebbaf83b3a49e23bf21722aa65f15606af2
Brandon Satrom <brsatrom@microsoft.com>
2011-09-18 15:48:34 -0700

    got SO working

--- js/landingPage.js
+++ js/landingPage.js
@@ -106,6 +106,7 @@
     // The getGroups() and getItems() functions contain sample data.
     // TODO: Replace with custom data.
     function getGroups() {
+        var colors = ['rgba(209, 211, 212, 1)', 'rgba(147, 149, 152, 1)', 'rgba(65, 64, 66, 1)'];
         var groups = [];
         
         groups.push({
@@ -114,46 +115,34 @@
             label: "Stack Overflow Tag Search",
             resource: {
                 url: "http://api.stackoverflow.com/1.1/search?tagged=",
+                contentType: "json",
                 delimiter: ";",
                 params: ["winrt", "winjs", "win8", "metrostyle", "dev11"]
             },
-            backgroundColor: "rgba(209, 211, 212, 1)",
+            callback: processSOData,
+            backgroundColor: colors[0],
             description: "Stack Overflow Tag Search",
             fullDescription: "Stack Overflow Tag Search (WinRT, win8, Metro Style, WinJS)"
         });
 
-        //groups.push({
-        //    key: "channel9Build",
-        //    title: "BUILD on Channel9",
-        //    label: "BUILD on Channel9",
-        //    resource: { url: "http://channel9.msdn.com/Events/BUILD/BUILD2011/RSS" },
-        //    backgroundColor: "",
-        //    description: "BUILD content on Channel 9",
-        //    fullDescription: "BUILD content on Channel 9"
-        //});
+        groups.push({
+            key: "channel9Build",
+            title: "BUILD on Channel9",
+            label: "BUILD on Channel9",
+            resource: {
+                url: "http://channel9.msdn.com/Events/BUILD/BUILD2011/RSS",
+                contentType: "xml",
+                params: []
+            },
+            callback: processC9Data,
+            backgroundColor: colors[1],
+            description: "BUILD content on Channel 9",
+            fullDescription: "BUILD content on Channel 9"
+        });
 
         return groups;
     } 
-
-    //function getGroups() {
-    //    var colors = ['rgba(209, 211, 212, 1)', 'rgba(147, 149, 152, 1)', 'rgba(65, 64, 66, 1)'];
-    //    var groups = [];
-
-    //    for (var i = 0; i < 6; i++) {
-    //        var even = (i % 2) === 0;
-    //        groups.push({
-    //            key: 'group' + i,
-    //            title: 'Collection title lorem ' + i,
-    //            backgroundColor: colors[i % colors.length],
-    //            label: 'Eleifend posuere',
-    //            description: even ? 'ǺSed nisl nibh, eleifend posuere.' : 'ǺSed nisl nibh, eleifend posuere laoreet egestas, porttitor quis lorem.',
-    //            fullDescription: 'Ǻ Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.' + (even ? '' : ' Ǻ Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.')
-    //        });
-    //    }
-
-    //    return groups;
-    //}
-
+        
     function getItems() {
         var colors = ['rgba(209, 211, 212, 1)', 'rgba(147, 149, 152, 1)', 'rgba(65, 64, 66, 1)'];
         var items = [];
@@ -166,28 +155,10 @@
                 groupUrl += group.resource.params[k] + (k+1 !== group.resource.params.length ? group.resource.delimiter : '');
             }
 
-            WinJS.xhr({ url: groupUrl }).then(function (request) {
-                JSON.parse(request);
-                items.push({
-                    group: pageData.groups[k],
-                    key: request
-                });
-            }, downloadError);
-        }
-
-        for (var g = 0, gl = pageData.groups.length; g < gl; g++) {
-            var numItems = g % 2 === 0 ? 6 : 4;
-            for (var i = 0; i < numItems; i++) {
-                items.push({
-                    group: pageData.groups[g],
-                    key: 'item' + i,
-                    title: g + '.' + i + (i % 2 === 0 ? ' ǺSed nisl nibh, eleifend posuere.' : ' ǺSed nisl nibh, eleifend posuere laoreet egestas, porttitor quis lorem.'),
-                    subtitle: 'Phasellus faucibus',
-                    backgroundColor: colors[i % colors.length],
-                    content: (new Array(16)).join('<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>'),
-                    description: 'Ǻ Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'
-                });
-            }
+            WinJS.xhr({ url: groupUrl }).then(group.callback, downloadError).then(function () {
+                var lv = ui.getControl(document.querySelector('.landingList'));
+                lv.dataSource = items;
+            });
         }
 
         return items;
@@ -214,8 +185,31 @@
     }
 
     pageData.groups = getGroups();
-    pageData.items = getItems();
+    getItems();
+    pageData.items = [];
 
+    function processSOData(request) {
+        var response = JSON.parse(request.responseText);
+        var length = response.questions.length > 8 ? 8 : response.questions.length;
+        for (var m = 0; m < length; m++) {
+            var question = response.questions[m];
+            items.push({
+                group: group,
+                key: question.question_id,
+                title: question.title,
+                subtitle: "(Asked by " + question.owner.display_name + " [" + question.owner.reputation + "])",
+                //content: "",
+                //description: "",
+                backgroundColor: colors[m % colors.length]
+            });
+        }
+
+        return items;
+    }
+
+    function processC9Data(request) {
+    }
+    
     WinJS.Namespace.define('landingPage', {
         fragmentLoad: fragmentLoad,
         itemInvoked: itemInvoked
